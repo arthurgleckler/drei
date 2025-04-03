0c87ac81569a7edecd35547bfd6da8842b0f0e80
Arthur A. Gleckler <aag@alum.mit.edu>
2025-04-03 14:08:17 -0700

    Reformat comment.  Punctuate properly.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Prevent additional console window on Windows in release.
+// DO NOT REMOVE.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri_plugin_app;
