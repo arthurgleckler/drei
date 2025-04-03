ff82136c2fb21b48c198f57d63bd3a441838be9f
Arthur A. Gleckler <aag@alum.mit.edu>
2025-04-02 21:49:39 -0700

    Fix some key bindings.  Make C-q exit.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_app;
+
 fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_app::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
     web_editor_lib::run()
 }
