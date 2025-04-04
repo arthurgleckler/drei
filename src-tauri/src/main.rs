7e5d9dd1275f6b4f47b3f246594b2e9d44281100
Arthur A. Gleckler <aag@alum.mit.edu>
2025-04-03 20:45:05 -0700

    Fetch contents of a blog entry to edit.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,10 +2,17 @@
 // DO NOT REMOVE.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
 use tauri_plugin_app;
+
+#[tauri::command]
+fn read_file(path: String) -> Result<String, String> {
+    fs::read_to_string(path).map_err(|e| e.to_string())
+}
 
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_file])
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_app::init())
         .run(tauri::generate_context!())
